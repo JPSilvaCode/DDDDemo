3328854f65449cf02659d22a9bfdce8925a02a82
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-17 17:19:19 -0300

    Remove Usings

--- DDDDemo.Dominio/Interfaces/Repositorio/IProdutoRepository.cs
+++ DDDDemo.Dominio/Interfaces/Repositorio/IProdutoRepository.cs
@@ -1,10 +1,6 @@
 ﻿using DDDDemo.Dominio.Entidades;
 using DDDDemo.Dominio.Interfaces.Repositorio.Base;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DDDDemo.Dominio.Interfaces.Repositorio
 {
