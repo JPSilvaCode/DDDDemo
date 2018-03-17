99994931a3c19932c8352c0252d03b2399422741
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-17 16:50:57 -0300

    Add Interfaces Repository: Produto e Categoria

---
+++ DDDDemo.Dominio/Interfaces/Repositorio/ICategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Repositorio.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Dominio.Interfaces.Repositorio
+{
+    public interface ICategoriaRepository : IRepositoryBase<Categoria>
+    {
+    }
+}
---
+++ DDDDemo.Dominio/Interfaces/Repositorio/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Repositorio.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Dominio.Interfaces.Repositorio
+{
+    public interface IProdutoRepository : IRepositoryBase<Produto>
+    {
+        IEnumerable<Produto> BuscarPorNome(string nome);
+    }
+}
