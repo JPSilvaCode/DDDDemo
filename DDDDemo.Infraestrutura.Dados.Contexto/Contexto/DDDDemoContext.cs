b7f8e02b15fea93609c467dfcb614bdc23e4bc6b
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-20 14:38:25 -0300

    Add Entity Framework Context

---
+++ DDDDemo.Infraestrutura.Dados.Contexto/Contexto/DDDDemoContext.cs
@@ -0,0 +1,36 @@
+﻿using DDDDemo.Dominio.Entidades;
+using DDDDemo.Infraestrutura.Dados.Contexto.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Infraestrutura.Dados.Contexto.Contexto
+{
+    public class DDDDemoContext : DbContext
+    {
+        public DDDDemoContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Configurations.Add(new CategoriaMap());
+            modelBuilder.Configurations.Add(new ProdutoMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
