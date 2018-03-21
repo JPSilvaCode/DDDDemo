06c3db22c54dd64eabcf670cb55879abbfabd298
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-21 08:23:30 -0300

    Add App Service Base Aplicação

---
+++ DDDDemo.Aplicacao/Base/AppServiceBase.cs
@@ -0,0 +1,50 @@
+﻿using DDDDemo.Aplicacao.Interfaces.Base;
+using DDDDemo.Dominio.Interfaces.Servico.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Aplicacao.Base
+{
+    public class AppServiceBase<TEntity> : IDisposable, IAppServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IServiceBase<TEntity> _serviceBase;
+
+        public AppServiceBase(IServiceBase<TEntity> serviceBase)
+        {
+            _serviceBase = serviceBase;
+        }
+
+        public void Add(TEntity obj)
+        {
+            _serviceBase.Add(obj);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _serviceBase.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _serviceBase.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _serviceBase.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _serviceBase.Update(obj);
+        }
+    }
+}
