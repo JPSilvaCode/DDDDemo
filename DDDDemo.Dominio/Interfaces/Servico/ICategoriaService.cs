0c3c1de583db9654a99a57f87a86ca1ee31e7d62
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-21 07:37:19 -0300

    Add Interfaces Service: Produto e Categoria

---
+++ DDDDemo.Dominio/Interfaces/Servico/ICategoriaService.cs
@@ -0,0 +1,14 @@
+﻿using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Servico.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Dominio.Interfaces.Servico
+{
+    public interface ICategoriaService : IServiceBase<Categoria>
+    {
+    }
+}
