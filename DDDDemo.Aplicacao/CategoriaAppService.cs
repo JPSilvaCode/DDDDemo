13bf319d67a2255a69136e9e2c993db2bb6d5c5e
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-21 08:31:36 -0300

    Add App Serviços Aplicação: Produto e Categoria

---
+++ DDDDemo.Aplicacao/CategoriaAppService.cs
@@ -0,0 +1,23 @@
+﻿using DDDDemo.Aplicacao.Base;
+using DDDDemo.Aplicacao.Interfaces;
+using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Aplicacao
+{
+    public class CategoriaAppService : AppServiceBase<Categoria>, ICategoriaAppService
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriaAppService(ICategoriaService categoriaService)
+           : base(categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+    }
+}
---
+++ DDDDemo.Aplicacao/ProdutoAppService.cs
@@ -0,0 +1,28 @@
+﻿using DDDDemo.Aplicacao.Base;
+using DDDDemo.Aplicacao.Interfaces;
+using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Aplicacao
+{
+    public class ProdutoAppService : AppServiceBase<Produto>, IProdutoAppService
+    {
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoAppService(IProdutoService produtoService)
+            : base(produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return _produtoService.BuscarPorNome(nome);
+        }
+    }
+}
