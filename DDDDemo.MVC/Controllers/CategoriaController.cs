adbf3d9621d5e974816cef3fa08c87811f1302f5
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-21 15:49:26 -0300

    Add Controller Categoria

---
+++ DDDDemo.MVC/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using DDDDemo.Aplicacao.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDDDemo.MVC.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ICategoriaAppService _categoriaAppService;
+
+        public CategoriaController(ICategoriaAppService categoriaAppService)
+        {
+            _categoriaAppService = categoriaAppService;
+        }
+
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            var categorias = _categoriaAppService.GetAll();
+            return View(categorias);
+        }
+    }
+}
