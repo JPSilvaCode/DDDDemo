7186ed15b8f5a1b82670c48e04c4d34aa0c8abdf
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-21 16:57:04 -0300

    Add Crud Categoria

--- DDDDemo.MVC/Controllers/CategoriaController.cs
+++ DDDDemo.MVC/Controllers/CategoriaController.cs
@@ -1,4 +1,5 @@
 ﻿using DDDDemo.Aplicacao.Interfaces;
+using DDDDemo.Dominio.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,77 @@ namespace DDDDemo.MVC.Controllers
         {
             var categorias = _categoriaAppService.GetAll();
             return View(categorias);
+        }
+
+        // GET: Categoria/Details/5
+        public ActionResult Details(int id)
+        {
+            var categoria = _categoriaAppService.GetById(id);            
+
+            return View(categoria);
+        }
+
+        // GET: Categoria/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categoria/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {               
+                _categoriaAppService.Add(categoria);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        // GET: Categoria/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var categoria = _categoriaAppService.GetById(id);            
+
+            return View(categoria);
+        }
+
+        // POST: Categoria/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoriaAppService.Update(categoria);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var categoria = _categoriaAppService.GetById(id);            
+
+            return View(categoria);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var categoria = _categoriaAppService.GetById(id);
+            _categoriaAppService.Remove(categoria);
+
+            return RedirectToAction("Index");
         }
     }
 }
