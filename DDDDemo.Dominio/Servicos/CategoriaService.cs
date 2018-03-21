3c696c2922e20563a916eda3fdfad8d085ab0a84
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-21 07:59:38 -0300

    Add Service Dominio: Produto e Categoria

---
+++ DDDDemo.Dominio/Servicos/CategoriaService.cs
@@ -0,0 +1,23 @@
+﻿using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Repositorio;
+using DDDDemo.Dominio.Interfaces.Servico;
+using DDDDemo.Dominio.Servicos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Dominio.Servicos
+{
+    public class CategoriaService : ServiceBase<Categoria> , ICategoriaService
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaService(ICategoriaRepository categoriaRepository)
+             : base(categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+    }
+}
---
+++ DDDDemo.Dominio/Servicos/ProdutoService.cs
@@ -0,0 +1,28 @@
+﻿using DDDDemo.Dominio.Entidades;
+using DDDDemo.Dominio.Interfaces.Repositorio;
+using DDDDemo.Dominio.Interfaces.Servico;
+using DDDDemo.Dominio.Servicos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDDemo.Dominio.Servicos
+{
+    public class ProdutoService : ServiceBase<Produto>, IProdutoService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoService(IProdutoRepository produtoRepository)
+             : base(produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return _produtoRepository.BuscarPorNome(nome);
+        }
+    }
+}
