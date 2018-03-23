1f8ab5c71b36d531267fb0adb470934511cc7196
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-03-23 11:45:29 -0300

    Test TDD - Categoria

---
+++ DDDDemo.Dominio.Tests/Entidades/CategoriaTests.cs
@@ -0,0 +1,33 @@
+﻿using DDDDemo.Dominio.Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace DDDDemo.Dominio.Tests.Entidades
+{
+    [TestClass]
+    public class CategoriaTests
+    {
+        [TestMethod]
+        public void Categoria_Consistente_True()
+        {
+            var categoria = new Categoria()
+            {
+                Nome = "João Paulo", //informando Nome Válido               
+            };
+
+            Assert.IsTrue(categoria.IsValid());
+        }
+
+        [TestMethod]
+        public void Aluno_Consistente_False()
+        {
+            var categoria = new Categoria()
+            {
+                Nome = "", //informando Nome Inválido               
+            };
+
+            Assert.IsFalse(categoria.IsValid());
+            Assert.IsTrue(categoria.ValidationResult.Erros.Any(e => e.Message == "O Nome é obrigatório"));
+        }
+    }
+}
--- DDDDemo.Dominio/Validations/CategoriaValid/CategoriaConsistenciaValid.cs
+++ DDDDemo.Dominio/Validations/CategoriaValid/CategoriaConsistenciaValid.cs
@@ -1,19 +1,14 @@
 ﻿using DDDDemo.Dominio.Entidades;
 using DDDDemo.Dominio.Specifications.CategoriaSpec;
 using DomainValidation.Validation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DDDDemo.Dominio.Validations.CategoriaValid
 {
     public class CategoriaConsistenciaValid : Validator<Categoria>
     {
         public CategoriaConsistenciaValid()
-        {
-            base.Add("Testando", new Rule<Categoria>(new CategoriaNomeObrigatorioSpec(), ValidationMessages.NomeIsRequired));
+        {           
+            base.Add("NomeCategoria", new Rule<Categoria>(new CategoriaNomeObrigatorioSpec(), ValidationMessages.NomeIsRequired));
         }
     }
 }
